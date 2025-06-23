935c605896dd9c6aec15cc9058ef96eee54a8812
yonagumo <yonagumo.dev@gmail.com>
2025-06-23 22:23:51 +0900

    v0.2.0 PyO3導入

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use dotenv::dotenv;
+use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 
+use pyo3::{prelude::*, types::IntoPyDict};
 use serenity::{async_trait, model::channel::Message, model::gateway::Ready, prelude::*};
 
-struct Handler;
+struct Handler {
+    google_api_key: String,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -12,7 +16,24 @@ impl EventHandler for Handler {
         if msg.author.bot {
             return;
         }
-        if let Err(why) = msg.channel_id.say(&ctx.http, "RustからHello, worldじゅう！").await {
+
+        // let reply = "RustからHello, worldじゅう！";
+        let reply: String = Python::with_gil(|py| {
+            let genai = py.import("google.genai")?;
+            let args = [("api_key", self.google_api_key.clone())].into_py_dict(py)?;
+            let client = genai.call_method("Client", (), Some(&args))?;
+            let mut args = HashMap::<&str, &str>::new();
+            args.insert("model", "gemini-2.5-flash");
+            args.insert("contents", &msg.content);
+            let response =
+                client
+                    .getattr("models")?
+                    .call_method("generate_content", (), Some(&args.into_py_dict(py)?))?;
+            Ok::<String, Box<dyn Error>>(response.getattr("text")?.to_string())
+        })
+        .unwrap();
+
+        if let Err(why) = msg.channel_id.say(&ctx.http, reply).await {
             println!("Error sending message: {:?}", why);
         }
     }
@@ -27,7 +48,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     dotenv()?;
     let token = env::var("DISCORD_TOKEN")?;
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::DIRECT_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(&token, intents).event_handler(Handler).await?;
+    let handler = Handler {
+        google_api_key: env::var("GOOGLE_API_KEY")?,
+    };
+    let mut client = Client::builder(&token, intents).event_handler(handler).await?;
     client.start().await?;
     Ok(())
 }
