1329ec0a0417c2dccf4bfc5a77bcaa98ff231a9b
yonagumo <yonagumo.dev@gmail.com>
2025-06-23 20:35:15 +0900

    v0.1.0 serenity導入

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::error::Error;
+use dotenv::dotenv;
+
+use serenity::{
+    async_trait,
+    model::channel::Message,
+    model::gateway::Ready,
+    prelude::*,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.author.bot {
+            return;
+        }
+        if let Err(why) = msg.channel_id.say(&ctx.http, "RustからHello, worldじゅう！").await {
+            println!("Error sending message: {:?}", why);
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>>{
+    dotenv()?;
+    let token = env::var("DISCORD_TOKEN")?;
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::DIRECT_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(&token, intents).event_handler(Handler).await?;
+    client.start().await?;
+    Ok(())
 }
