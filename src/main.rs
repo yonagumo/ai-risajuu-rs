20999ad07a08e3c096bd23de645c49473bb33570
yonagumo <yonagumo.dev@gmail.com>
2025-06-24 15:54:30 +0900

    v0.3.0 gemini-rsに変更

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use dotenv::dotenv;
-use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 
-use pyo3::{prelude::*, types::IntoPyDict};
-use serenity::{async_trait, model::channel::Message, model::gateway::Ready, prelude::*};
+use gemini_rs::{self as gemini};
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
 
 struct Handler {
-    google_api_key: String,
+    client: gemini_rs::Client,
+}
+
+impl Handler {
+    fn new(api_key: &str) -> Handler {
+        let client = gemini::Client::new(api_key);
+        Handler { client }
+    }
 }
 
 #[async_trait]
@@ -17,24 +27,17 @@ impl EventHandler for Handler {
             return;
         }
 
+        msg.channel_id.broadcast_typing(&ctx.http).await.expect("typing error");
         // let reply = "RustからHello, worldじゅう！";
-        let reply: String = Python::with_gil(|py| {
-            let genai = py.import("google.genai")?;
-            let args = [("api_key", self.google_api_key.clone())].into_py_dict(py)?;
-            let client = genai.call_method("Client", (), Some(&args))?;
-            let mut args = HashMap::<&str, &str>::new();
-            args.insert("model", "gemini-2.5-flash");
-            args.insert("contents", &msg.content);
-            let response =
-                client
-                    .getattr("models")?
-                    .call_method("generate_content", (), Some(&args.into_py_dict(py)?))?;
-            Ok::<String, Box<dyn Error>>(response.getattr("text")?.to_string())
-        })
-        .unwrap();
+        let mut route = self.client.generate_content("gemini-2.5-flash");
+        route.message(&msg.content);
+        let response = route.await.expect("request error");
+        let text = response.candidates[0].content.parts[0].text.clone();
 
-        if let Err(why) = msg.channel_id.say(&ctx.http, reply).await {
-            println!("Error sending message: {:?}", why);
+        if let Some(reply) = text {
+            if let Err(why) = msg.channel_id.say(&ctx.http, reply).await {
+                println!("Error sending message: {:?}", why);
+            }
         }
     }
 
@@ -48,10 +51,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     dotenv()?;
     let token = env::var("DISCORD_TOKEN")?;
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::DIRECT_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
-    let handler = Handler {
-        google_api_key: env::var("GOOGLE_API_KEY")?,
-    };
-    let mut client = Client::builder(&token, intents).event_handler(handler).await?;
+    let handler = Handler::new(&env::var("GOOGLE_API_KEY")?);
+    let mut client = serenity::Client::builder(&token, intents).event_handler(handler).await?;
     client.start().await?;
     Ok(())
 }
