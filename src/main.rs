ff0920e0defd1463d9bc198b1767f08807ac847d
yonagumo <yonagumo.dev@gmail.com>
2025-06-25 23:30:53 +0900

    v0.4.1 システム指示と履歴を追加

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use dotenv::dotenv;
 use futures::{channel::mpsc, try_join};
 use std::env;
 use std::error::Error;
+use std::fs::File;
+use std::io::Read;
 
 mod discord;
 mod risajuu;
@@ -20,7 +22,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let api_key = env::var("GOOGLE_API_KEY")?;
     let model_name = env::var("AI_MODEL")?;
-    let mut risajuu = Risajuu::new(&api_key, receiver, model_name);
+
+    let mut f = File::open("src/system_instruction.md")?;
+    let mut system_instruction = String::new();
+    f.read_to_string(&mut system_instruction)?;
+
+    let mut risajuu = Risajuu::new(&api_key, receiver, model_name, system_instruction);
 
     try_join!(discord.run(), risajuu.run())?;
     Ok(())
--- src/risajuu.rs
+++ src/risajuu.rs
@@ -3,25 +3,54 @@ use std::error::Error;
 use futures::channel::mpsc::UnboundedReceiver;
 
 use futures::StreamExt;
-use gemini_rs::Client;
-use gemini_rs::Error as GeminiError;
+use gemini_rs::{
+    Client, Error as GeminiError,
+    types::{
+        Content, HarmBlockThreshold,
+        HarmCategory::{self, *},
+        Part, Role, SafetySettings,
+    },
+};
 use serenity::{client::Context, model::channel::Message};
 
 type Receiver = UnboundedReceiver<(Context, Message)>;
+
+const HARM_CATEGORIES: [HarmCategory; 5] = [
+    HarmCategoryHarassment,
+    HarmCategoryHateSpeech,
+    HarmCategorySexuallyExplicit,
+    HarmCategoryDangerousContent,
+    HarmCategoryCivicIntegrity,
+];
 
 pub struct Risajuu {
     receiver: Receiver,
     gemini: Client,
     model_name: String,
+    history: Vec<Content>,
+    system_instruction: String,
+    safety_settings: Vec<SafetySettings>,
 }
 
 impl Risajuu {
-    pub fn new(api_key: &str, receiver: Receiver, model_name: String) -> Risajuu {
+    pub fn new(api_key: &str, receiver: Receiver, model_name: String, system_instruction: String) -> Risajuu {
         let gemini = gemini_rs::Client::new(api_key);
+        let history = Vec::new();
+        let mut safety_settings = Vec::new();
+        for hc in HARM_CATEGORIES {
+            let ss = SafetySettings {
+                category: hc,
+                threshold: HarmBlockThreshold::BlockNone,
+            };
+            safety_settings.push(ss);
+        }
         Risajuu {
             receiver,
             gemini,
             model_name,
+            history,
+            system_instruction,
+            safety_settings,
         }
     }
 
@@ -34,8 +63,11 @@ impl Risajuu {
             };
 
             if let Some(text) = reply {
-                if let Err(why) = msg.channel_id.say(&ctx.http, text).await {
-                    println!("Error (say): {:?}", why);
+                let mut iter = text.split('\n').filter(|line| !line.is_empty());
+                while let Some(chunk) = iter.next() {
+                    if let Err(why) = msg.channel_id.say(&ctx.http, chunk).await {
+                        println!("Error (say): {:?}", why);
+                    }
                 }
             }
         }
@@ -44,8 +76,24 @@ impl Risajuu {
 
     async fn chat(&mut self, msg: &str) -> Result<Option<String>, GeminiError> {
         let mut request = self.gemini.generate_content(&self.model_name);
+        request.safety_settings(self.safety_settings.clone());
+        request.system_instruction(&self.system_instruction);
+        request.contents(self.history.clone());
         request.message(msg);
         let response = request.await?;
-        Ok(response.candidates[0].content.parts[0].text.clone())
+        let response = response.candidates[0].content.parts[0].text.clone();
+        let user_content = Content {
+            role: Role::User,
+            parts: vec![Part::text(msg)],
+        };
+        self.history.push(user_content);
+        if let Some(text) = &response {
+            let model_content = Content {
+                role: Role::Model,
+                parts: vec![Part::text(text)],
+            };
+            self.history.push(model_content);
+        };
+        Ok(response)
     }
 }
